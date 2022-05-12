f30dd9fd5ea8e5053d8d078fad536137ee4a3fa2
Sivaprathap S <102897756+tringapps-sivaprathap@users.noreply.github.com>
2022-05-12 17:28:55 +0530

    questions and options displayed

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Quiz from "./components/Quiz";
 import StartQuiz from "./components/StartQuiz";
 
 const App = () => {
     const [StartQuizFlag, setStartQuizFlag] = useState(false);
-    const [quizData, setQUizData] = useState([]);
-
-useEffect(() => { // move fetch to quiz page
-    fetch(`https://opentdb.com/api.php?amount=5&type=multiple`)
-      .then((response) => response.json())
-      .then((data) => {
-            let tempQuizData = data.results.map((element) => {
-            let options = [...element.incorrect_answers, element.correct_answer];
-            for (let i = options.length -1; i > 0; i--) {
-            let j = Math.floor(Math.random() * i)
-            let k = options[i]
-            options[i] = options[j]
-            options[j] = k
-            }
-            return {
-            question: element.question,
-            correct_answer: element.correct_answer,
-            options
-            }
-            });
-            setQUizData(tempQuizData);
-      })
-}, [])
 
     return (
         <div className="App">
             {!StartQuizFlag && <StartQuiz setStartQuizFlag={setStartQuizFlag} />}
             {StartQuizFlag && <Quiz />}
-            {/* {quizData.map((item) => (
-                {renderHTML("<p>item.question</p>")}
-            ))} */}
         </div>
     );
 }
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,11 +1,65 @@
 import { useEffect, useState } from "react";
 
 const Quiz = () => {
-    
+    const [quizData, setQUizData] = useState([]);
+    const [points, setPoints] = useState(0);
+    const [submitted, setSubmitted] = useState(false);
+
+    useEffect(() => {
+        fetch(`https://opentdb.com/api.php?amount=5&type=multiple`)
+          .then((response) => response.json())
+          .then((data) => {
+                let tempQuizData = data.results.map((element) => {
+                    let options = [...element.incorrect_answers, element.correct_answer];
+                    for (let i = options.length -1; i > 0; i--) {
+                        let j = Math.floor(Math.random() * i)
+                        let k = options[i]
+                        options[i] = options[j]
+                        options[j] = k
+                    }
+                    return {
+                        question: element.question,
+                        correct_answer: element.correct_answer,
+                        options,
+                        clickedOption: '',
+                        result: false
+                    }
+                });
+                setQUizData(tempQuizData);
+          })
+    }, [])
+
+    const handleClick = (event, item) => {
+        // console.log(event.target.value, '---', item.correct_answer);
+
+        setQUizData(quizData.map((element) => {
+            if(event.target.value === item.correct_answer) return ({...element, clickedOption: event.target.value, result: true});
+            else return ({...element});
+        }));
+
+        console.log(quizData);
+    }
 
     return (
         <div>
-            Questions
+            {quizData.map(item => {
+                return (
+                    <div key={quizData.indexOf(item)}>
+                        <p>{item.question}</p>
+                        {item.options.map(option => {
+                            return (
+                                submitted ?
+                                (<span key={item.options.indexOf(option)} value = {option}>{option}</span>) :
+                                (<button key={item.options.indexOf(option)} value = {option} onClick={(event) => {handleClick(event, item)}}>{option}</button>)
+                            )
+                        })}
+                    </div>
+                )
+            })}
+
+            <button onClick={() => {setSubmitted(true)}}>Submit</button>
+
+            <p>{JSON.stringify(quizData)}</p>
         </div>
     );
 }
